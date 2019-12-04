40a2a3b21b3a413e6c9fa8a5fb4652812d67007e
WideWebVR <presciente.snotrq@gmail.com>
2019-12-04 15:47:40 +0100

    Mention sempai in Readme

--- basics/exercices/chap_2/bool.c
+++ basics/exercices/chap_2/bool.c
@@ -1,5 +1,21 @@
-
+#include <stdio.h>
 /*
 Write a program that reads an input number; answer "1" if it is odd, "0" if it is even
-
 */
+
+int main(void){
+  int input;
+  short int output;
+  printf("Please enter a number : \n");
+  scanf("%d", &input);
+
+  if (input % 2 == 0) {
+    output = 0;
+  } 
+  else {
+    output = 1;
+  }
+
+  printf("%hi \n", output);
+  return 0;
+}
